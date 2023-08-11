1ad1a17841708f1e1075ffc9405e947026156c47
a <a@a.a>
2023-08-11 22:13:02 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='yzw-dl',
-    version='1.0.0-20230715',
+    version='1.0.0-20230811',
     author='xx025',
     packages=['yzw_dl', 'yzw_dl.utils','yzw_dl.data'],
     install_requires=[
