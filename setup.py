950bc99e4ba40a38cf8337211ac03da553df9c17
a <a@a.a>
2023-08-11 22:31:54 +0800

    调整进度在0-100 之间

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='yzw-dl',
-    version='1.0.0-20230811',
+    version='1.0.0-202308111',
     author='xx025',
     packages=['yzw_dl', 'yzw_dl.utils','yzw_dl.data'],
     install_requires=[
